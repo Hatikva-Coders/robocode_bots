7762bbbf786cbc7e935a78f03a73d599ed93f8db
aryehpro <aryehpro@gmail.com>
2016-06-04 18:53:54 +0300

    not sure what this is, but whatever

---
+++ src/org/aryehpro/robocode/LoopExperiment.java
@@ -0,0 +1,16 @@
+package org.aryehpro.robocode;
+
+/**
+ * Created by Aryeh on 13/01/2016.
+ */
+public class LoopExperiment {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i = i +2 ) {
+            System.out.println(i);
+        }
+
+    }
+
+}
